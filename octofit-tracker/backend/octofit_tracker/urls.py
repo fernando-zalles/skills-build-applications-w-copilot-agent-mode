ea4c22053f61264c83f91a8894877fda81da1f69
fernando-zalles <juanferzalles2323@gmail.com>
2026-03-17 04:29:30 +0000

    comment

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -1,4 +1,5 @@
-"""octofit_tracker URL Configuration
+"""
+octofit_tracker URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
